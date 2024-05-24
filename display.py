460764627731e47cac0c8c183248c1ba5dc31e70
BxsedSosa <donovanxabad@gmail.com>
2024-05-24 10:20:01 -0700

    Ignore this code

--- display.py
+++ display.py
@@ -6,8 +6,24 @@ from pyfiglet import Figlet
 BORDER = "=" * 65
 logo_font = Figlet(font="larry3d")
 game_font = Figlet(font="banner")
+game_board = [["-"] * 3 for _ in range(3)]
 
 
 def clear_console() -> None:
     """Clear console from text"""
     system("clear||cls")
+
+
+def display_logo() -> None:
+    print(logo_font.renderText("TicTacToe"))
+    print(BORDER)
+
+
+def display_grid(grid) -> None:
+    for row in grid:
+        print("  ".join(game_font.renderText(row)))
+
+
+def display_game() -> None:
+    display_logo()
+    display_grid(game_board)
--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-"""Imports"""
+from display import display_game
 
-from display import display_game, update_board
-from game_logic import ask_player_position, check_for_winner_round
-
-board = [["-"] * 3 for _ in range(3)]
-
-while True:
-    player, cpu = 0, 0
-
-    display_game()
-    player_move = ask_player_position()
-    update_board(player_move)
-    result = check_for_winner_round()
-
-    if result == "player":
-        player += 1
-    elif result == "cpu":
-        cpu += 1
-
-    if player == 3:
-        break
-    elif cpu == 3:
-        break
+display_game()
